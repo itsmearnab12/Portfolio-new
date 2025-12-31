5537466e322d3807130d21440cbca9794d6d9e3c
Arnab <arnabchoudhury2005@gmail.com>
2025-12-31 19:42:35 +0530

    learning and building

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,7 +1,26 @@
-export function AboutPage(){
-    return(
+import "./Aboutpage.css"
+export function AboutPage() {
+    return (
         <>
-            About Page 
+            <div className="Aboutpage">
+                <h5>-About Page</h5>
+                <div className="Aboutme">
+                    <h3>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur, doloribus. Quasi alias autem amet voluptate accusamus dolores repellendus ipsa, possimus nulla mollitia iure iste magni, quibusdam ut maiores quas. Quasi dolor nemo quas saepe quaerat consectetur. Corporis, non tempore doloremque atque quo, dignissimos, repellendus reiciendis odit quae recusandae voluptatem blanditiis. Laboriosam adipisci commodi rem voluptatem!</h3>
+                </div>
+
+                <div className="Aboutme">
+                    <h3>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam voluptas facere id amet eos quisquam magni ea dignissimos sapiente aliquid? Id aut magnam, cumque illum nulla sed rerum provident delectus inventore. Magni aspernatur ea totam dolor suscipit, quaerat fuga sunt, hic reprehenderit quidem consectetur odit corrupti eos quasi, aut explicabo officia unde temporibus! Cum, dolores!
+                    </h3>
+                </div>
+
+                <div className="Aboutme">
+                    <h3>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. A fugit eveniet commodi nobis libero nam eligendi nulla laborum. Illo corporis ab quibusdam laudantium velit vel, eaque porro repellat asperiores, voluptatibus perferendis natus ad. Cumque molestiae eveniet id doloribus, eaque expedita facilis iusto, ea amet qui possimus maiores fuga. Sunt at praesentium dolore, odit quidem est.
+                    </h3>
+                </div>
+            </div>
+
         </>
     );
 }
