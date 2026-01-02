261f843b457ad60fab183f9cf416b64a7b6e218e
Arnab <arnabchoudhury2005@gmail.com>
2026-01-02 21:46:10 +0530

    Learing and building

--- src/Pages/ExperiencePage.jsx
+++ src/Pages/ExperiencePage.jsx
@@ -1,11 +1,9 @@
-import { GithubCalendar } from "../Components/GithubCalendar";
 import { Techstack } from "../Components/Techstack";
 
 export function ExperiencePage(){
     return(
         <>
             <Techstack />
-            <GithubCalendar />
         </>
     );
 }
