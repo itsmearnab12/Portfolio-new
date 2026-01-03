8db37df9dccbbe5a8aa8ae5b63ba53bf89c91855
Arnab <arnabchoudhury2005@gmail.com>
2026-01-03 19:44:58 +0530

    Learing and Building

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,7 +1,12 @@
+import "./Contactpage.css"
+import { FaHandshake } from "react-icons/fa6";
 export function ContactPage(){
     return(
         <>
-            Contact Page
+            <div className="contactpage">
+                <h5>- Get in Touch</h5>
+                <h1>Open to learning opportunities and collaborations <FaHandshake /></h1>
+            </div>
         </>
     );
 }
