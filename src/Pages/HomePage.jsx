a0c252985a7d8d6653d62c12f0281be0eef0ec21
Arnab <arnabchoudhury2005@gmail.com>
2026-01-07 18:56:20 +0530

    Learning and Building

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -17,31 +17,33 @@ export function HomePage() {
 
     return (
         <>
-            <div className="Homepage">
-                <div className="HomepageRightsec">
-                    <h5>- Welcome To My Portfolio</h5>
-                    <h1>Hello</h1>
-                    <h1>I'm <span>Arnab</span></h1>
-                    <h1>
-                        <Hero />
-                    </h1>
-                    <h4>I’m a third-year BCA (Hons.) student at Parul University with a strong interest in software development. I enjoy turning ideas into real-world applications and have a growing passion for working with data.</h4>
-                    <div className="Button">
-                        <button className="Resumebutton"><IoDocumentTextOutline />View Resume</button>
-                        <button onClick={() => navigate("/Project")} className="Myworkbutton">My Work <FaArrowRightLong /></button>
+            <section className="section home-section">
+                <div className="Homepage">
+                    <div className="HomepageRightsec">
+                        <h5>- Welcome To My Portfolio</h5>
+                        <h1>Hello</h1>
+                        <h1>I'm <span>Arnab</span></h1>
+                        <h1>
+                            <Hero />
+                        </h1>
+                        <h4>I’m a third-year BCA (Hons.) student at Parul University with a strong interest in software development. I enjoy turning ideas into real-world applications and have a growing passion for working with data.</h4>
+                        <div className="Button">
+                            <button className="Resumebutton"><IoDocumentTextOutline />View Resume</button>
+                            <button onClick={() => navigate("/Project")} className="Myworkbutton">My Work <FaArrowRightLong /></button>
+                        </div>
+                        <div className="Homesocialmedia">
+                            <a href="https://www.linkedin.com/in/arnab-choudhury12/" target="_blank"><FaLinkedin /></a>
+                            <a href="https://github.com/itsmearnab12" target="_blank"><FaGithub /></a>
+                            <a href="mailto:arnabchoudhury2005@gmail.com" target="_blank"><FiMail /></a>
+                            <a href="https://x.com/itsarnab12" target="_blank"><FaSquareXTwitter /></a>
+                        </div>
                     </div>
-                    <div className="Homesocialmedia">
-                        <a href="https://www.linkedin.com/in/arnab-choudhury12/" target="_blank"><FaLinkedin /></a>
-                        <a href="https://github.com/itsmearnab12" target="_blank"><FaGithub /></a>
-                        <a href="mailto:arnabchoudhury2005@gmail.com" target="_blank"><FiMail /></a>
-                        <a href="https://x.com/itsarnab12" target="_blank"><FaSquareXTwitter /></a>
+                    <div className="HomepageLeftsec">
+                        <img src={Profile} alt="Profile" />
                     </div>
                 </div>
-                <div className="HomepageLeftsec">
-                    <img src={Profile} alt="Profile" />
-                </div>
-            </div>
-            <div className="Aboutpage">
+            </section>
+            <section className="section Aboutpage">
                 <h5 className="AboutTilte">- About Me</h5>
                 <h2>Change My idea to relaity</h2>
                 <h4 className="AboutMyself">
@@ -51,48 +53,48 @@ export function HomePage() {
                 <div>
                     <a href="">Know more about me <FaArrowRightLong /> </a>
                 </div>
-            </div>
-            <div>
+            </section>
+            <section className="section tech-section">
                 <Techstack />
-            </div>
-            <div className="projectpage">
+            </section>
+            <section className="section projectpage">
                 <h5>-My Projects</h5>
                 <h2>Code. Build. Improve.</h2>
 
                 <div className="project-cards">
                     <Projectcard
-                    Title="Chatapp"
-                    Development="FULLSTACK"
-                    Discription="A real-time chat application that allows users to communicate instantly, focusing on performance, usability, and a responsive design."
-                    Tech="React.js Express.js Node.js Tailwind Css Socket.io"
-                    Livelink="https://chat-app-wine-seven-52.vercel.app/"
-                    Github="https://github.com/itsmearnab12/Chatapp"
-                />
-                <Projectcard
-                    Title="News Website"
-                    Development="FULLSTACK"
-                    Discription="A dynamic news platform built to fetch and display real-time news with a modern and user-friendly interface."
-                    Tech="HTML Express.js Node.JS CSS News.io"
-                    Livelink="https://chat-app-wine-seven-52.vercel.app/"
-                    Github="https://github.com/itsmearnab12/FullStackProjects/tree/main/Project%201"
-                />
-                <Projectcard
-                    Title="Simple Portfolio website"
-                    Development="FRONTEND"
-                    Discription="A dynamic news platform built to fetch and display real-time news with a modern and user-friendly interface."
-                    Tech="HTML Express.js Node.JS CSS News.io"
-                    Livelink="https://itsmearnab12.github.io/Portfolio/"
-                    Github="https://github.com/itsmearnab12/Portfolio"
-                />
-                <Projectcard
-                    Title="News Website"
-                    Discription="A dynamic news platform built to fetch and display real-time news with a modern and user-friendly interface."
-                    Tech="HTML Express.js Node.JS CSS News.io"
-                    Livelink="https://chat-app-wine-seven-52.vercel.app/"
-                    Github="https://github.com/itsmearnab12/FullStackProjects/tree/main/Project%201"
-                />
+                        Title="Chatapp"
+                        Development="FULLSTACK"
+                        Discription="A real-time chat application that allows users to communicate instantly, focusing on performance, usability, and a responsive design."
+                        Tech="React.js Express.js Node.js Tailwind Css Socket.io"
+                        Livelink="https://chat-app-wine-seven-52.vercel.app/"
+                        Github="https://github.com/itsmearnab12/Chatapp"
+                    />
+                    <Projectcard
+                        Title="News Website"
+                        Development="FULLSTACK"
+                        Discription="A dynamic news platform built to fetch and display real-time news with a modern and user-friendly interface."
+                        Tech="HTML Express.js Node.JS CSS News.io"
+                        Livelink="https://chat-app-wine-seven-52.vercel.app/"
+                        Github="https://github.com/itsmearnab12/FullStackProjects/tree/main/Project%201"
+                    />
+                    <Projectcard
+                        Title="Simple Portfolio website"
+                        Development="FRONTEND"
+                        Discription="A dynamic news platform built to fetch and display real-time news with a modern and user-friendly interface."
+                        Tech="HTML Express.js Node.JS CSS News.io"
+                        Livelink="https://itsmearnab12.github.io/Portfolio/"
+                        Github="https://github.com/itsmearnab12/Portfolio"
+                    />
+                    <Projectcard
+                        Title="News Website"
+                        Discription="A dynamic news platform built to fetch and display real-time news with a modern and user-friendly interface."
+                        Tech="HTML Express.js Node.JS CSS News.io"
+                        Livelink="https://chat-app-wine-seven-52.vercel.app/"
+                        Github="https://github.com/itsmearnab12/FullStackProjects/tree/main/Project%201"
+                    />
                 </div>
-            </div>
+            </section>
         </>
     );
 }
