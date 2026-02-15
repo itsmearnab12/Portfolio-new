6b9695a6c3852ca2afad2450fa06ff24fc158690
Arnab <arnabchoudhury2005@gmail.com>
2026-02-15 15:40:10 +0530

    Learning and Building

--- src/Pages/CertificatePage.jsx
+++ src/Pages/CertificatePage.jsx
@@ -1,12 +1,8 @@
-import CertificateSlider from "../Components/CertificateSlider";
-import "./Certificatepage.css"
-
 export function CertificatePage() {
     return (
         <>
             <div className="Certificatehead">
                 <h2>My Certification</h2>
-                <CertificateSlider />
             </div>
         </>
     );
