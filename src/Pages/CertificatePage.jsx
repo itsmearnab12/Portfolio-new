6b3d853255b635c69bbc1f891ea939f381dbe04a
Arnab <arnabchoudhury2005@gmail.com>
2026-02-16 08:30:07 +0530

    Learning and Building

--- src/Pages/CertificatePage.jsx
+++ src/Pages/CertificatePage.jsx
@@ -1,9 +1,9 @@
-export function CertificatePage() {
-    return (
+import "./Certificatepage.css"
+
+export function CertificatePage(){
+    return(
         <>
-            <div className="Certificatehead">
-                <h2>My Certification</h2>
-            </div>
+        <h3>Working on Certififate page</h3>
         </>
     );
 }
